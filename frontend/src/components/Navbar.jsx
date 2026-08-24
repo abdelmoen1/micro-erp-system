c054198fa3d1a39ac113737121de2c334e1bb60e
abdelmoen1 <aboodbarbari543@gmail.com>
2026-08-24 16:47:55 +0200

    feat(ui): implement reusable Navbar component using JSX  - Create a responsive and reusable Navbar component. - Utilize JSX syntax for component structure and styling.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="container">
+        <a href="#" className="logo-link">
+          <div className="logo-icon">M</div>
+          <span>Micro ERP</span>
+        </a>
+        <ul className="nav-links">
+          <li>
+            <a href="#features">Features</a>
+          </li>
+
+          <li>
+            <a href="#why">Why us?</a>
+          </li>
+
+          <li>
+            <a href="#plans">Prices</a>
+          </li>
+
+          <li>
+            <a href="#faq">F&Q</a>
+          </li>
+        </ul>
+        <div className="nav-actions">
+          <button>العربية</button>
+
+          <a href="/login">Login</a>
+
+          <a href="/register" className="signup-button">
+            Get Started
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
