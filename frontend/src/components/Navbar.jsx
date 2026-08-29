a83abe172aa1f6edeabc5c0cb4cc29c8701176f6
abdelmoen1 <aboodbarbari543@gmail.com>
2026-08-29 13:31:42 +0200

    feat(ui): mobile navbar menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,36 +4,80 @@ const links = [
   ["prices", "#plans"],
   ["faq", "#faq"],
 ];
+import { useState } from "react";
 function Navbar({ t, lang, setLang }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <nav>
-      <div className="container">
-        <a href="#" className="logo-link">
-          <div className="logo-icon">M</div>
-          <span>Micro ERP</span>
-        </a>
-        <ul className="nav-links">
+    <>
+      {" "}
+      <nav>
+        <div className="container">
+          <a href="#" className="logo-link">
+            <div className="logo-icon">M</div>
+            <span>Micro ERP</span>
+          </a>
+          <ul className="nav-links">
+            <>
+              {links.map(([key, href]) => (
+                <li key={key}>
+                  <a href={href}>{t.nav[key]}</a>
+                </li>
+              ))}
+            </>
+          </ul>
+          <div className="nav-actions">
+            <button onClick={() => setLang(lang === "en" ? "ar" : "en")}>
+              {lang === "en" ? "العربية" : "English"}
+            </button>
+
+            <a href="/login">{t.nav.login}</a>
+
+            <a href="/register" className="register-button">
+              {t.nav.signup}
+            </a>
+            <button
+              className="mobile-only-btn"
+              aria-expanded={isOpen}
+              aria-label="Menu"
+              onClick={toggleMenu}
+            >
+              {isOpen === false ? "☰" : "✕"}
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div
+        className="mobile-menu-container"
+        style={
+          isOpen === true ? { visibility: "visible" } : { visibility: "hidden" }
+        }
+      >
+        <ul>
+          {" "}
           <>
             {links.map(([key, href]) => (
               <li key={key}>
-                <a href={href}>{t.nav[key]}</a>
+                <a href={href} className="menu-link">
+                  {t.nav[key]}
+                </a>
               </li>
             ))}
           </>
         </ul>
-        <div className="nav-actions">
-          <button onClick={() => setLang(lang === "en" ? "ar" : "en")}>
-            {lang === "en" ? "العربية" : "English"}
-          </button>
-
-          <a href="/login">{t.nav.login}</a>
-
-          <a href="/register" className="register-button">
-            {t.nav.signup}
+        <div className="menu-actions">
+          <a href="/login" className="btn-login">
+            تسجيل الدخول
+          </a>
+          <a href="/register" className="btn-register">
+            ابدأ مجاناً
           </a>
         </div>
       </div>
-    </nav>
+    </>
   );
 }
 
